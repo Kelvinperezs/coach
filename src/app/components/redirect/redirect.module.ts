76fdbc9ba1d8bdbafcf4303b3bc15215de764358
Kelvinperezs <47196960+Kelvinperezs@users.noreply.github.com>
2021-11-11 08:51:38 -0500

    Create redirect.module.ts

---
+++ src/app/components/redirect/redirect.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RedirectComponent } from './redirect.component';
+
+
+
+@NgModule({
+  declarations: [
+    RedirectComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RedirectModule { }
